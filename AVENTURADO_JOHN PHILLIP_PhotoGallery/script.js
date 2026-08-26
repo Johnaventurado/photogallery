0fd96a357058a919dd5923ba3641b6ee61a3094e
John Aventurado <johnaventurado07@gmail.com>
2026-08-26 13:34:26 +0800

    Add files via upload

---
+++ AVENTURADO_JOHN PHILLIP_PhotoGallery/script.js
@@ -0,0 +1,262 @@
+const photos = {
+
+    1: {
+        image: "picture/3987470a-e982-4f13-b41a-8bdf8ccbf13c.jpg",
+        title: "Sunset Vibes",
+        description:
+            "This photo captures one of my favorite sunset moments. The warm colors of the sky and the peaceful atmosphere made this moment special to me. It reminds me that sometimes the simplest moments can become some of the best memories.",
+        location: "NAGA City, Philippines",
+        date: "MAY 19, 2026",
+        category: "Personal"
+    },
+
+
+    2: {
+        image: "picture/2b108c37-b905-4965-bc81-27b7ab5e612d.jpg",
+        title: "Fitnes Gym",
+        description:
+            "This is my the picture when I was in the fitness era.",
+        location: "Lahug Cebu, Philippines",
+        date: "June 8, 2026",
+        category: "Personal"
+    },
+
+
+    3: {
+        image: "picture/af84185c-bb88-472b-a52b-dd29a2e016ad.jpg",
+        title: "With Friends",
+        description:
+            "Spending time with friends always creates memories worth keeping while on duty As a RESCUE/EMS. This picture represents friendship, laughter, and the simple moments that make life more enjoyable.",
+        location: "Cebu City, Philippines",
+        date: "August 5, 2026",
+        category: "Friends"
+    },
+
+
+    4: {
+        image: "picture/1786210465004.jpeg",
+        title: "Freedom on Two Wheels",
+        description:
+            "Riding gives me a feeling of freedom and adventure. Every road has a different view and every ride becomes another story that can be remembered.",
+        location: "SRP Cebu, Philippines",
+        date: "August 3, 2026",
+        category: "Adventure"
+    },
+
+
+    5: {
+        image: "picture/1786663542806.jpeg",
+        title: "City Lights",
+        description:
+            "The city looks completely different when the lights begin to shine. This photo captures the feeling of exploring a busy place while enjoying the view around me.",
+        location: "Busay Cebu City, Philippines",
+        date: "August 1, 2026",
+        category: "Travel"
+    },
+
+
+    6: {
+        image: "picture/1786663744105.jpeg",
+        title: "Beach Day",
+        description:
+            "A relaxing day near the sea is always something worth remembering. The sound of the waves, fresh air, and beautiful scenery made this a memorable experience.",
+        location: "SAn Rem Cebu, Philippines",
+        date: "July 18, 2026",
+        category: "Travel"
+    },
+
+
+    7: {
+        image: "picture/HA.jpeg",
+        title: "Food Trip",
+        description:
+        "One of the simple joys in life is enjoying good food. This photo captures a delicious meal that made the day more enjoyable and memorable.",
+        location: "Cebu City, Philippines",
+        date: "July 25, 2026",
+        category: "Food"
+    },
+
+
+    8: {
+        image: "picture/647df787-974e-4501-b54b-f3049424a98e.jpg",
+        title: "Special Day",
+        description:
+            "This was a special day filled with happiness and unforgettable memories.I took this picture while on duty as a rescue at the same time was birthday It is one of those moments that I would always want to look back on.",
+        location: "Cebu City, Philippines",
+        date: "July 15, 2026",
+        category: "Special Moment"
+    },
+
+
+    9: {
+        image: "picture/walking.jpeg",
+        title: "Morning Walk",
+        description:
+            "A peaceful morning walk gave me the opportunity to enjoy the surroundings and start the day with a positive mindset.",
+        location: "Cebu City, Philippines",
+        date: "July 19, 2026",
+        category: "Daily Life"
+    },
+
+
+    10: {
+        image: "picture/685147970_1268769278746887_1536030675704746701_n.jpg",
+        title: "Weekend Adventure",
+        description:
+            "Weekends are a great opportunity to explore new places, experience something different, and create memories outside of the usual routine.",
+        location: "Toledo City Cebu, Philippines",
+        date: "June 30, 2026",
+        category: "Adventure"
+    },
+
+
+    11: {
+        image: "picture/681361064_1472610234308941_8797226302010920926_n.jpg",
+        title: "Good Memories",
+        description:
+            "This photograph captures a simple but meaningful moment. Sometimes the moments that seem ordinary become the memories we appreciate the most.",
+        location: "Toledo City Cebu, Philippines",
+        date: "June 30, 2026",
+        category: "Memories"
+    },
+
+
+    12: {
+        image: "picture/ssstik_1780823503146.jpeg",
+        title: "Golden Hour",
+        description:
+            "The golden hour creates some of the most beautiful natural lighting. This moment was peaceful and beautiful enough to deserve a place in my gallery.",
+        location: "Cebu, Philippines",
+        date: "July 10, 2026",
+        category: "Nature"
+    },
+
+
+    13: {
+        image: "picture/679793239_863192596796868_6043346864762142379_n.jpg",
+        title: "Adventure Time",
+        description:
+            "Every adventure gives us an opportunity to experience something new. Picture capture while praying before the rides This was another memorable part of my journey.",
+        location: "Lahug Cebu, Philippines",
+        date: "July 8, 2026",
+        category: "Adventure"
+    },
+
+
+    14: {
+        image: "picture/1e5ed237-adeb-4333-bbf5-2d14e4418869.jpg",
+        title: "Nature",
+        description:
+            "Nature has a way of making everything feel calm. This photograph captures a peaceful moment surrounded by natural beauty.",
+        location: "Cebu, Philippines",
+        date: "July 5, 2026",
+        category: "Nature"
+    },
+
+
+    15: {
+        image: "picture/683142158_1005351455774206_6064218755979867182_n.jpg",
+        title: "Good Times",
+        description:
+            "Good times become great memories when shared with the people around us. This is one moment that I will always appreciate.",
+        location: "Cebu City TCH, Philippines",
+        date: "July 2, 2026",
+        category: "Friends"
+    },
+
+
+    16: {
+        image: "picture/730775725_1390054832969053_7262759407451342730_n.jpg",
+        title: "Travel Memories",
+        description:
+            "Traveling allows us to discover new places and create experiences that stay with us. This photograph represents one of those journeys.",
+        location: "Philippines",
+        date: "June 28, 2026",
+        category: "Travel"
+    },
+
+
+    17: {
+        image: "picture/686103869_1487385079547652_105583487039469668_n.jpg",
+        title: "Friends Forever",
+        description:
+            "Friendship is one of the most valuable parts of life. This picture represents the fun, laughter, and memories created with good friends.",
+        location: "Cebu City, Philippines",
+        date: "June 25, 2026",
+        category: "Friends"
+    },
+
+
+    18: {
+        image: "picture/SnapInsta.to_673820500_18352978399300891_3785086358205318978_n.jpg",
+        title: "Peaceful Place",
+        description:
+            "Sometimes we simply need a quiet place to relax and appreciate the moment. This photo represents peace and simplicity.",
+        location: "Capisan Zamboanga Del Sur, Philippines",
+        date: "June 20, 2026",
+        category: "Nature"
+    },
+
+
+    19: {
+        image: "picture/708852726_1321653526611074_1460874012629955949_n (1).jpg",
+        title: "Another Chapter",
+        description:
+            "Every new experience becomes another chapter in life. This photograph represents moving forward and creating new memories.",
+        location: "Cebu City, Philippines",
+        date: "June 15, 2026",
+        category: "Life"
+    },
+
+
+    20: {
+        image: "picture/576321006_26038542759068891_7075152101458428758_n.png",
+        title: "Forever Remembered",
+        description:
+            "Some moments deserve to be remembered forever. This photograph captures one of those meaningful experiences.",
+        location: "Cebu, Philippines",
+        date: "AROUND OCTOBER when typhoon Tino hit",
+        category: "Memories"
+    }
+
+};
+
+
+// Get the photo number from the URL
+
+const params = new URLSearchParams(window.location.search);
+
+const photoNumber = params.get("photo");
+
+
+// Find the selected photo
+
+const selectedPhoto = photos[photoNumber];
+
+
+// Display the information
+
+if (selectedPhoto) {
+
+    document.getElementById("detailImage").src =
+        selectedPhoto.image;
+
+    document.getElementById("detailImage").alt =
+        selectedPhoto.title;
+
+    document.getElementById("detailTitle").textContent =
+        selectedPhoto.title;
+
+    document.getElementById("detailDescription").textContent =
+        selectedPhoto.description;
+
+    document.getElementById("detailLocation").textContent =
+        selectedPhoto.location;
+
+    document.getElementById("detailDate").textContent =
+        selectedPhoto.date;
+
+    document.getElementById("detailCategory").textContent =
+        selectedPhoto.category;
+
+}
